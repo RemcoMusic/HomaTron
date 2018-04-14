e605e03c5b1696a8b8872e360208bfb9914b0386
Remco van Gorsel <0925836@hr.nl>
2018-04-14 17:06:05 +0200

    Eerste versie werkend waarbij een Device met ID 1 een lamp aan en uit kan zetten

--- Database/src/main/java/HomaTron/Database/databaseConnection.java
+++ Database/src/main/java/HomaTron/Database/databaseConnection.java
@@ -5,15 +5,18 @@ import org.json.JSONArray;
 
 public class databaseConnection 
 {
-	private boolean setUpdateCheck = true;
-	private JSONArray array1 = new JSONArray();
+	private JSONArray defaultArray = new JSONArray();
+	private JSONArray newArray = new JSONArray();
+	
+	private String Database = "HomaTron";
+	private String DeviceID = "DeviceID";
+	private String Status = "Online";
 	
 	public databaseConnection()
 	{
-		
 	}
 	
-	public void checkForUpdate() //This method will check if there is an update in the Database
+	public boolean checkForUpdate(boolean setUpdateCheck) //This method will check if there is an update in the Database
 	{
 		try
 		{  
@@ -21,7 +24,7 @@ public class databaseConnection
 			while(setUpdateCheck)
 			{
 				Class.forName("com.mysql.jdbc.Driver");  
-				Connection con=DriverManager.getConnection("jdbc:mysql://83.86.242.110:3306/webapp","HomaTron","HomaTron");  
+				Connection con=DriverManager.getConnection("jdbc:mysql://83.86.242.110:3306/" + Database,"HomaTron","HomaTron");  
 			
 				Statement stmt=con.createStatement();  
 				ResultSet rs=stmt.executeQuery("select * from status");  
@@ -39,46 +42,65 @@ public class databaseConnection
 		catch(Exception e)
 		{ 
 			System.out.println(e);
-		}  
+		} 
+		return true;
 	}
 	
 	public void pullTables() //This method will pull the new data from the Database when this is updated
-	{
+	{	
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");  
-			Connection con=DriverManager.getConnection("jdbc:mysql://83.86.242.110:3306/webapp","HomaTron","HomaTron");  
+			Connection con=DriverManager.getConnection("jdbc:mysql://83.86.242.110:3306/" + Database,"HomaTron","HomaTron");  
 	
 			Statement stmt = con.createStatement();  
-			ResultSet rs = stmt.executeQuery("select * from outputdevice");  
+			ResultSet rs = stmt.executeQuery("select * from device");  
 	
+			JSONArray array = new JSONArray();
+			int deviceID;
+			int status;
+			
 			while(rs.next()) 
 			{ 
-				System.out.println(rs.getInt(1));  
+				JSONObject item = new JSONObject();
+				deviceID = rs.getInt(DeviceID);
+				status = rs.getInt(Status);
+				
+				item.put("DeviceID", deviceID);
+				item.put("Status", status);
+				array.put(item);
+				
+				//System.out.println(rs.getInt(1));  
 			}
 			
-			Statement stmt1 = con.createStatement();  
-			int rs1 = stmt1.executeUpdate("UPDATE `Test`.`status` SET `status`='0' WHERE `idstatus`='1'");
 			
-			setUpdateCheck = true;
-			checkForUpdate();
+			String message = array.toString();
+			
+			System.out.println();
+			System.out.println("databaseConnection: Inhoud van de new Array: " + message);
+			
+			setNewData(array);
+			
+			Statement stmt1 = con.createStatement();  
+			stmt1.executeUpdate("UPDATE `HomaTron`.`status` SET `status`='0' WHERE `status`='1'");
+			
 			con.close();		
 		}
 		catch(Exception e)
 		{ 
 			System.out.println(e);
-		}  
+		} 
 	}
 	
-	public void setDefaultData() //This method will set the default data on startup to check the differents
+	public void pullDefaultData() //This method will set the default data on startup to check the differents
 	{
 		try
 		{ 
 			Class.forName("com.mysql.jdbc.Driver");  
-			Connection con=DriverManager.getConnection("jdbc:mysql://83.86.242.110:3306/webapp","HomaTron","HomaTron");  
+			Connection con=DriverManager.getConnection("jdbc:mysql://83.86.242.110:3306/" + Database,"HomaTron","HomaTron");  
 	
 			Statement stmt = con.createStatement();  
-			ResultSet rs = stmt.executeQuery("select * from outputdevice");  
+			ResultSet rs = stmt.executeQuery("select * from device");  
 	
 			JSONArray array = new JSONArray();
 			int deviceID;
@@ -87,8 +109,8 @@ public class databaseConnection
 			while(rs.next()) 
 			{ 
 				JSONObject item = new JSONObject();
-				deviceID = rs.getInt("OutputDeviceID");
-				status = rs.getInt("Online");
+				deviceID = rs.getInt(DeviceID);
+				status = rs.getInt(Status);
 				
 				item.put("DeviceID", deviceID);
 				item.put("Status", status);
@@ -99,9 +121,8 @@ public class databaseConnection
 			
 			String message = array.toString();
 			
-			System.out.println("Inhoud van de Array: " + message);
-			
-			System.out.println(array.getJSONObject(0).get("DeviceID"));
+			System.out.println("databaseConnection: Inhoud van de default Array: " + message);
+			System.out.println();
 			
 			setDefaultData(array);
 			
@@ -115,12 +136,22 @@ public class databaseConnection
 	
 	public void setDefaultData(JSONArray array)
 	{
-		this.array1 = array;
+		this.defaultArray = array;
 	}
 	
 	public JSONArray getDefaultData()
 	{
-		return this.array1;
+		return this.defaultArray;
+	}
+	
+	public void setNewData(JSONArray array)
+	{
+		this.newArray = array;
+	}
+	
+	public JSONArray getNewData()
+	{
+		return this.newArray; 
 	}
 }
 
--- Database/src/main/java/HomaTron/Database/eventHandler.java
+++ Database/src/main/java/HomaTron/Database/eventHandler.java
@@ -3,18 +3,91 @@ import java.sql.*;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
+
+
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.Customization;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.comparator.CustomComparator;
+
 public class eventHandler 
 {
 	
 	private databaseConnection foo = new databaseConnection();
 	
 	
-	public eventHandler(){}
-	
-	public void test()
+	public eventHandler()
 	{
-		foo.setDefaultData();
-		System.out.println("Dit is de eventHandler: " + foo.getDefaultData());
+		foo.pullDefaultData();;
+		System.out.println("=====eventHandler=====");
+		System.out.println("eventHandler: Dit is de eventHandler default data : " + foo.getDefaultData());
+		System.out.println("=====eventHandler=====");
+		
+		receivedUpdate();
+	}
+	
+	public void receivedUpdate()
+	{
+		while(true)
+		{
+			if(foo.checkForUpdate(true))
+			{
+				System.out.println("=====eventHandler=====");
+				System.out.println("Dit is de eventHandler new data: " + foo.getNewData());
+				System.out.println("=====eventHandler=====");
+				System.out.println();
+				System.out.println("=====eventHandler=====");
+				System.out.println("Dit is de eventHandler default data: " + foo.getDefaultData());
+				System.out.println("=====eventHandler=====");
+				JSONArray actual = foo.getNewData();
+				JSONArray expected = foo.getDefaultData();
+			
+				try 
+				{
+					JSONAssert.assertNotEquals(expected.getJSONObject(1), actual.getJSONObject(1), true);
+					
+					System.out.println(expected.getJSONObject(1).get("DeviceID"));
+					System.out.println(actual.getJSONObject(1).get("DeviceID"));
+					
+					String fooo =  expected.getJSONObject(1).get("DeviceID").toString();
+					String foooo = actual.getJSONObject(1).get("DeviceID").toString();
+					
+					if(fooo.equalsIgnoreCase(foooo))
+					{
+						System.out.println("Komt hier iets uberhaupt?");
+						String x = actual.getJSONObject(1).get("Status").toString();
+						System.out.println();
+						System.out.println(x);
+						
+						if(x.equalsIgnoreCase("1"))
+						{
+							mqtt.publishToBroker("HomaTron", "00" + expected.getJSONObject(1).get("DeviceID") + ":003");
+						}
+						if(x.equalsIgnoreCase("0"))
+						{
+							mqtt.publishToBroker("HomaTron", "00" + expected.getJSONObject(1).get("DeviceID") + ":004");
+						}
+					}
+					
+					JSONObject test1 = new JSONObject();
+					test1 = expected.getJSONObject(1);
+					
+					String test = test1.get("DeviceID").toString();
+					
+					System.out.println();
+					System.out.println("Er is een actie gebeurd");
+					System.out.println();
+					System.out.println("Dit is de default id: " + test);
+					System.out.println("Dit is de new id: " + actual.getJSONObject(1).get("DeviceID"));
+				} 
+				catch (JSONException e) 
+				{
+					System.out.println("Het is niet gelukt neem ik aan ?????");
+					//e.printStackTrace();
+				}		
+			}
+		}
 	}
 	
 }
